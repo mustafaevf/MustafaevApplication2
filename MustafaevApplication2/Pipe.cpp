036bb2f681713f0ea1e88cd5e72830d25e319249
Farid Mustafaev <knetya274@gmail.com>
2022-10-31 23:46:15 +0300

    upd

--- MustafaevApplication2/Pipe.cpp
+++ MustafaevApplication2/Pipe.cpp
@@ -1,47 +1,54 @@
 #include "Pipe.h"
-#include "Utils.h"
+#include <fstream>
 
-unsigned int Pipe::IDp = 0;
+int Pipe::IDp = 0;
 
-int Pipe::getId() const
-{
-	return this->id;
+// get
+int Pipe::getId() const { return id; } 
+double Pipe::getLength() const { return length; }
+bool Pipe::getRepair() const { return repair; }
+double Pipe::getDiametr() const { return diametr; }
+std::string Pipe::getName() const { return name; }
+
+// set
+void Pipe::setName(std::string _name) { name = _name; }
+void Pipe::setLength(double _length) { length = _length; }
+void Pipe::setDiametr(double _diametr) { diametr = _diametr; }
+void Pipe::setRepair(bool _repair) { repair = _repair; }
+void Pipe::setId(int _id) { id = _id; }
+
+void Pipe::updateRepair() { repair = !repair; }
+
+void Pipe::Save(std::ofstream& fout) {
+	fout << name << std::endl;
+	fout << length << std::endl;
+	fout << diametr << std::endl;
+	fout << repair << std::endl;
 }
 
-double Pipe::getLength() const
-{
-	return this->length;
+void Pipe::Load(std::ifstream& fin) {
+	fin >> name;
+	fin >> length;
+	fin >> diametr;
+	fin >> repair;
 }
 
-bool Pipe::getRepair() const
-{
-	return this->repair;
-}
+//std::ostream& operator << (std::ostream& out, Pipe& pipe) {
+//	out << "Труба (" << pipe.id << ")" << std::endl;
+//	out << "Название: " << pipe.name << std::endl;
+//	out << "Длина: " << pipe.length << std::endl;
+//	out << "Диаметр: " << pipe.diametr << std::endl;
+//	out << (pipe.repair ? "В РЕМОНТЕ" : "В РАБОТЕ") << std::endl;
+//	return out;
+//}
 
-double Pipe::getDiametr() const
-{
-	return this->diametr;
-}
-
-void Pipe::setLength(double length)
-{
-	this->length = length;
-}
-
-void Pipe::setDiametr(double diametr)
-{
-	this->diametr = diametr;
-}
-
-void Pipe::setRepair(bool repair)
-{
-	this->repair = repair;
-}
-
-void Pipe::setId(int id)
-{
-	this->id = id;
-}
-void Pipe::updateRepair() {
-	this->repair = !this->repair;
-}
+//std::istream& operator >> (std::istream& in, Pipe& pipe) {
+//	pipe.setId(++Pipe::IDp);
+//	double length = getNumber("Введите длинну трубы: ", 1, 1000);
+//	double diametr = getNumber("Введите диаметр трубы: ", 1, 1000);
+//	bool repair = getNumber("В ремонте: ", 0, 1);
+//	pipe.setLength(length);
+//	pipe.setDiametr(diametr);
+//	pipe.setRepair(repair);
+//	return in;
+//}
--- MustafaevApplication2/Pipe.h
+++ MustafaevApplication2/Pipe.h
@@ -1,19 +1,39 @@
-#pragma once
+#include <iostream>
+#include <string>
+
 class Pipe {
+
 private:
-	unsigned int id;
+	int id;
+	std::string name;
 	double length;
 	double diametr;
 	bool repair;
 public:
-	static unsigned int IDp;
+	static int IDp;
+
+	// get
 	int getId() const;
 	double getLength() const;
 	bool getRepair() const;
 	double getDiametr() const;
+	std::string getName() const;
+
+	//set
 	void setLength(double length);
 	void setDiametr(double diametr);
 	void setRepair(bool repair);
+	void setName(std::string name);
 	void setId(int id);
+
+	//
 	void updateRepair();
+	//
+	void Save(std::ofstream& fout);
+	void Load(std::ifstream& fin);
+
+	/*friend std::ostream& operator << (std::ostream& out, Pipe& pipe);*/
+
+	/*friend std::ifstream& operator >> (std::ifstream& fin, Pipe& pipe);*/
+		
 };
