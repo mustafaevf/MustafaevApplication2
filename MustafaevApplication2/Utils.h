5caa4268a1e23ee4b757e7b86497050a284d9751
Farid Mustafaev <knetya274@gmail.com>
2022-10-31 23:47:37 +0300

    Фильтры

--- MustafaevApplication2/Utils.h
+++ MustafaevApplication2/Utils.h
@@ -1,6 +1,9 @@
-#pragma once
 #include <iostream>
 #include <string>
+#include <vector>
+#include <map>
+#include "Pipe.h"
+#include "Station.h"
 
 template <typename T>
 T getNumber(std::string text, T borderMin, T borderMax) {
@@ -19,5 +22,45 @@ T getNumber(std::string text, T borderMin, T borderMax) {
 	}
 }
 
+template <typename T>
+std::vector<int> SearchPipesByFilter(std::map<int, Pipe>& Pipes, int param_search, T parametr)
+{
+	std::vector<int> result;
+	for (auto& pipe : Pipes)
+	{
+		if (param_search == 1) {
+			if (pipe.second.getId() == parametr) {
+				result.push_back(pipe.second.getId());
+			}
+		}
+		if (param_search == 2) {
+			if (pipe.second.getRepair() == (bool)parametr) {
+				result.push_back(pipe.second.getId());
+			}
+		}
+		
+
+	}
+	return result;
+}
 
 
+std::vector<int> SearchStationsByFilter(std::map<int, Station>& Stations, std::string parametr) {
+	std::vector<int> result;
+	for (auto& station : Stations) {
+		if (station.second.getName() == parametr) {
+			result.push_back(station.second.getId());
+		}
+	}
+	return result;
+}
+
+template <typename T>
+std::vector<int> SearchStationsByFilter(std::map<int, Station>& Stations, T parametr) {
+	std::vector<int> result;
+	for (auto& station : Stations) {
+		if (station.second.getEfficiency() == parametr)
+			result.push_back(station.second.getId());
+	}
+	return result;
+}
